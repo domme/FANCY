9028031afe2c46a6d2d2e35d42952375e169bf68
Dominik Lazarek <dominik.lazarek@gmail.com>
2016-02-01 22:20:23 +0100

    Sync
    Former-commit-id: dc72edb7871188b17da87cef30bad4ffb907e578

--- fancy_core/src/GpuBufferDX12.cpp
+++ fancy_core/src/GpuBufferDX12.cpp
@@ -38,14 +38,40 @@ namespace Fancy { namespace Rendering { namespace DX12 {
 
     GpuBufferCreationParams* pBaseParams = &myParameters;
     *pBaseParams = someParameters;
-
-    D3D12_HEAP_TYPE heapType = 
-      someParameters.ePrimaryUsageType == GpuBufferUsage::CONSTANT_BUFFER ?
-      D3D12_HEAP_TYPE_UPLOAD : D3D12_HEAP_TYPE_DEFAULT;
-
-    if (someParameters.uAccessFlags & 
     
-    // D3D12_HEAP_PROPERTIES heapProps;
+    const bool wantsCpuWrite = (someParameters.uAccessFlags & (uint)GpuResourceAccessFlags::WRITE) > 0u;
+    const bool wantsCpuRead = (someParameters.uAccessFlags & (uint)GpuResourceAccessFlags::READ) > 0u;
+
+    // Can we do something with these flags on DX12?
+    //const bool wantsDynamic = (someParameters.uAccessFlags & (uint)GpuResourceAccessFlags::DYNAMIC) > 0u;
+    //const bool wantsCoherent = (someParameters.uAccessFlags & (uint)GpuResourceAccessFlags::COHERENT) > 0u;
+    //const bool wantsCpuStorage = (someParameters.uAccessFlags & (uint)GpuResourceAccessFlags::PREFER_CPU_STORAGE) > 0u;
+    
+    D3D12_HEAP_TYPE heapType = D3D12_HEAP_TYPE_DEFAULT;
+    D3D12_CPU_PAGE_PROPERTY cpuPageProp = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+    D3D12_RESOURCE_STATES resourceStates = D3D12_RESOURCE_STATE_COMMON;
+
+    if (wantsCpuWrite && wantsCpuRead)
+    {
+      heapType = D3D12_HEAP_TYPE_UPLOAD;  // could also select _READBACK here...
+      cpuPageProp = D3D12_CPU_PAGE_PROPERTY_WRITE_BACK;  // For CPU-reads, we need the data as soon as possible
+      
+    }
+    else if(wantsCpuWrite && !wantsCpuRead)
+    {
+      heapType = D3D12_HEAP_TYPE_UPLOAD;
+      cpuPageProp = D3D12_CPU_PAGE_PROPERTY_WRITE_COMBINE;
+    }
+    else if(!wantsCpuWrite && wantsCpuRead)
+    {
+      heapType = D3D12_HEAP_TYPE_READBACK;
+      cpuPageProp = D3D12_CPU_PAGE_PROPERTY_WRITE_BACK;
+    }
+
+
+
+    
+    D3D12_HEAP_PROPERTIES heapProps;
     // heapProps.
 
 
