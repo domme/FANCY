2bbff3f0f5abb18d4a031afa3fbab089e8750c6e
Dominik <Dominik.lazarek@gmail.com>
2022-01-09 13:01:41 +0100

    Fix issue with glm include

--- fancy_core/MathIncludes.h
+++ fancy_core/MathIncludes.h
@@ -2,6 +2,7 @@
 
 //Math includes
 #define GLM_FORCE_RADIANS 1
+#define GLM_FORCE_CTOR_INIT 1  // We want e.g. matrices to be default-constructed to the identity, which we rely on in some places
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
 #include <glm/gtx/compatibility.hpp>
